f0a5892e6728223dd08e0ae0522e8b82786fe77c
AlfonsoDiaz09 <alfonsodiazc.09@gmail.com>
2026-02-12 09:23:45 -0600

    feat: Custom appbar

---
+++ lib/ui/widgets/app_bar/appbar_help.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppbarHelp extends StatelessWidget {
+  const AppbarHelp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.all(8.w),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white.withValues(alpha: 0.05),
+        ),
+        child: const Icon(Icons.help_outline, color: Colors.white),
+      ),
+    );
+  }
+}
---
+++ lib/ui/widgets/app_bar/custom_appbar.dart
@@ -0,0 +1,54 @@
+import 'package:dynora_finance/ui/bloc/navigation/navigation_bloc.dart';
+import 'package:dynora_finance/ui/widgets/app_bar/appbar_help.dart';
+import 'package:dynora_finance/ui/widgets/app_bar/appbar_notifications.dart';
+import 'package:dynora_finance/ui/widgets/app_bar/appbar_profile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+final excludedRoutesAppbar = [];
+
+final includedRoutesAppbar = [];
+
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppbar({super.key, this.height});
+
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocSelector<NavigationBloc, NavigationState, String?>(
+      selector: (state) {
+        if (excludedRoutesAppbar.contains(state.currentRoute)) {
+          return null;
+        }
+        return state.currentRoute;
+      },
+      builder: (context, state) {
+        if (state == null) {
+          return SizedBox.shrink();
+        }
+        return AppBar(
+          toolbarHeight: height ?? 60.h,
+          backgroundColor: Color(0xFF1B0234),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              AppbarProfile(),
+              Row(
+                children: [
+                  AppbarNotifications(),
+                  SizedBox(width: 12.w),
+                  AppbarHelp(),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(height ?? 60.h);
+}
